65777820d4b04fed67be1e62cd494bf238533345
Gabriel Paredes <paredesgabrielb@gmail.com>
2023-11-21 03:42:48 +0100

    First Commit: Structure

---
+++ TodoList/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  TodoList
+//
+//  Created by Gabriel Paredes on 21/11/23.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
