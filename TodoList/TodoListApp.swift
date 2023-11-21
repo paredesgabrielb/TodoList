8bee4989278c2c5129ddd133c7dd9bdad737e4f8
Gabriel Paredes <paredesgabrielb@gmail.com>
2023-11-21 02:35:41 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Gabriel Paredes on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
